907a8ce1953fa5c371062f7dfcc6eaca7176af96
cranburyattic <phirow@Phils-MBP.home>
2016-07-23 14:14:00 +0100

    Report tidy up

--- python/daily-market-report.py
+++ python/daily-market-report.py
@@ -10,42 +10,33 @@ from jinja2 import Environment, FileSystemLoader
 
 with open('../config/betfair_config.yml', 'r') as f:
     doc = yaml.load(f)
-    data_dir = doc["default"]["betfair"]["data_dir"]
+    data_dir = doc['default']['betfair']['data_dir']
 
-todays_date = strftime("%Y-%-m-%d", gmtime())
-
-
-
+todays_date = strftime('%Y-%-m-%d', gmtime())
 
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template("report.html")
+template = env.get_template('report.html')
 
-
-markets = pd.read_csv(data_dir + "/data/" + todays_date+ "/markets.csv",dtype={'marketId':str})
-scatter = pd.read_csv(data_dir + "/data/" + todays_date+ "/scatter.csv", header=None)
+markets = pd.read_csv(data_dir + '/data/' + todays_date+ '/markets.csv',dtype={'marketId':str})
+scatter = pd.read_csv(data_dir + '/data/' + todays_date+ '/scatter.csv', header=None)
 
 distance_and_runners_df = markets[['distance','runners']]
 
-grouped = distance_and_runners_df.groupby(['distance', 'runners']);
-
-list(grouped.count());
-
 distance_and_runners = distance_and_runners_df.plot(kind='bar')
 
-template_vars = {"title" : "Market Report - " + strftime("%Y-%m-%d %H:%M:%S", localtime()),
-                 "race_list": markets.to_html(),
-                 "summary": markets.describe().to_html()}
-
+template_vars = {'title' : 'Market Report - ' + strftime('%Y-%m-%d %H:%M:%S', localtime()),
+                 'race_list': markets.to_html(),
+                 'summary': markets.describe().to_html()}
 
 html_out = template.render(template_vars)
 
-distance_and_runners.get_figure().savefig("/tmp/distance-and-runners.png")
+distance_and_runners.get_figure().savefig('/tmp/distance-and-runners.png')
 
 fig, ax = plt.subplots()
 
-ax.set_title("RACES",fontsize=14)
-ax.set_xlabel("DISTANCE",fontsize=12)
-ax.set_ylabel("RUNNERS",fontsize=12)
+ax.set_title('RACES',fontsize=14)
+ax.set_xlabel('DISTANCE',fontsize=12)
+ax.set_ylabel('RUNNERS',fontsize=12)
 ax.grid(True,linestyle='-',color='0.75')
 
 yActA = np.random.randn(10)
