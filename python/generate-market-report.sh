ceaa7641bf2066c2df0cf36136c6974e655d9fa7
Phil Rowley <phirow@phils-imac.home>
2016-07-23 20:33:58 +0100

    Update shell script to work in launchctl

--- python/generate-market-report.sh
+++ python/generate-market-report.sh
@@ -1,3 +1,5 @@
-python3 daily-market-report.py > /tmp/out-temp.html
-wkhtmltopdf /tmp/out-temp.html /tmp/market-report.pdf
-python3 copy-market-report.py
+#! /bin/bash
+
+/usr/local/bin/python3 daily-market-report.py > /tmp/out-temp.html
+/usr/local/bin/wkhtmltopdf /tmp/out-temp.html /tmp/market-report.pdf
+/usr/local/bin/python3 copy-market-report.py
