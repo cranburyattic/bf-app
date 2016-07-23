62336edad7edc7d2bffe2d77031d479198a769ef
cranburyattic <phirow@Phils-MBP.home>
2016-07-23 13:55:26 +0100

    Update to python3

--- python/generate-market-report.sh
+++ python/generate-market-report.sh
@@ -1,3 +1,3 @@
-python daily-market-report.py > /tmp/out-temp.html
+python3 daily-market-report.py > /tmp/out-temp.html
 wkhtmltopdf /tmp/out-temp.html /tmp/market-report.pdf
-python copy-market-report.py
+python3 copy-market-report.py
