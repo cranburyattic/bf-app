07c94068f415e4199fe09b39fdad815c4bb0b673
Phil Rowley <phirow@phils-imac.home>
2016-08-14 19:07:10 +0100

    Bug fixes

---
+++ extract-market-data-from-betfair.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+/usr/local/bin/node /Users/phirow/Development/git-repo/bf-app/ca_getDailyRacingMarkets.js
+/usr/local/bin/node /Users/phirow/Development/git-repo/bf-app/ca-get-daily-under25-markets.js
---
+++ run-racing-markets.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+/usr/local/bin/node /Users/phirow/Development/git-repo/bf-app/ca-run-racing-markets.js
